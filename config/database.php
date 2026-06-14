08f766c989308aac6bf4d97ae88e9386729eb6ab
Your Name <you@example.com>
2026-06-14 20:27:09 +1000

    first commit

--- config/database.php
+++ config/database.php
@@ -71,7 +71,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'search_path' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => env('DB_SSLMODE', 'prefer'),
         ],
 
         'sqlsrv' => [
