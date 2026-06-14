f1117ebc4ea4dadf4d0c26fd238e4f7a43ad7e13
Your Name <you@example.com>
2026-06-15 01:53:00 +1000

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,7 @@ set -e
 cd /var/www/html
 
 # Ждем PostgreSQL
-echo "Waiting for PostgreSQL..."
-until php artisan migrate:status >/dev/null 2>&1
-do
-  echo "Waiting for PostgreSQL..."
-  sleep 5
-done
+
 
 php artisan migrate --force
 
@@ -18,7 +13,6 @@ php artisan config:clear
 php artisan config:cache
 
 # Миграции
-php artisan migrate --force
 
 # Запуск
 php artisan serve --host=0.0.0.0 --port=10000
